cd1396f82a0228207dbed1f66d48ca490ac9f9c7
Joshua Wulf <sitapati@worldsankirtan.net>
2016-12-13 22:40:40 +1000

    Create docker-ubuntu.sh

---
+++ docker-ubuntu.sh
@@ -0,0 +1,3 @@
+sudo apt-get update
+sudo apt-get upgrade
+curl -s https://gist.githubusercontent.com/luislobo/2dc3de67b7f2ddc623c239dff36962a0/raw/9f24ff62eb7ada78718f5a805b54c0295c248692/install_latest_docker_compose.sh | bash /dev/stdin
