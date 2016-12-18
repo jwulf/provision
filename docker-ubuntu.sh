bb66742103c48e3506e4233f97dfb5a195ee4371
Joshua Wulf <sitapati@worldsankirtan.net>
2016-12-18 13:34:12 +1000

    Update docker-ubuntu.sh

--- docker-ubuntu.sh
+++ docker-ubuntu.sh
@@ -1,4 +1,5 @@
 echo ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC3jfmIXnZR6+LRWSNC353Ip6iNsz3LK0/Rbvowzx4lYdHd8rjhXocIlAygtoRET2/eS/Kl9GL3p4uHtKpLDFTVRNnd+ZuMuRfczbqs4vkPLpZJ5SD1jlebNpRLmh4FrJ8+sOptb4eWEuIqRFVV2u3gWkSSAFMGdEY/kW3xq73r1SH52WvH1wXWfveGiosBCZRggN1iUdHQ8ikXJ7nIjgz6T5kBSVvTB3h3gEA3vRRobAsb8rPU1c2DrCaiUNlo6emFxiOfjADt8mMOrHGjzZzCrwEkYqpxs0GH5gnX1zp1OAlK2qAoBUub7Vet2yu2inaDjGhG+vuzjXA9PlpqlrtP sitapati@Sitapatis-MacBook-Air.local >> ~/.ssh/authorized_keys 
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y upgrade
+sudo apt-get -y install python
 curl -s https://gist.githubusercontent.com/luislobo/2dc3de67b7f2ddc623c239dff36962a0/raw/9f24ff62eb7ada78718f5a805b54c0295c248692/install_latest_docker_compose.sh | bash /dev/stdin
